0090e73f0b5735e7249c4cdbe8d55c146ca4dd19
Alan <tallmanjohn@bitbucket.org>
2013-04-29 13:36:55 +0100

    adding webstorm project

---
+++ roughwork.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Alan
+ * Date: 29/04/13
+ * Time: 13:35
+ * To change this template use File | Settings | File Templates.
+ */
+
+console.log("something cool ")
